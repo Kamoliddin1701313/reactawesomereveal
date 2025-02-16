de93fa67f42ba0996b659342dcc10184fa2f5e5f
Kamoliddin1701313 <kamol170professor@gmail.com>
2025-02-16 21:02:43 +0500

    add qildim yana

--- src/Pages/Portfoliya/MyWorks/MyWorks.jsx
+++ src/Pages/Portfoliya/MyWorks/MyWorks.jsx
@@ -12,6 +12,21 @@ import app9 from "../portfoliyaData/app-9.jpg";
 import app10 from "../portfoliyaData/app-10.jpg";
 import app11 from "../portfoliyaData/app-11.jpg";
 import app12 from "../portfoliyaData/app-12.jpg";
+
+// photo1,photo2,photo3,photo4,photo5,photo6,photo7,photo8,photo9,photo10,photo11,photo12
+import photo1 from "../portfoliyaData/photo1.jpg";
+import photo2 from "../portfoliyaData/photo2.jpg";
+import photo3 from "../portfoliyaData/photo3.jpg";
+import photo4 from "../portfoliyaData/photo4.jpg";
+import photo5 from "../portfoliyaData/photo5.jpg";
+import photo6 from "../portfoliyaData/photo6.jpg";
+import photo7 from "../portfoliyaData/photo7.jpg";
+import photo8 from "../portfoliyaData/photo8.jpg";
+import photo9 from "../portfoliyaData/photo9.jpg";
+import photo10 from "../portfoliyaData/photo10.jpg";
+import photo11 from "../portfoliyaData/photo11.jpg";
+import photo12 from "../portfoliyaData/photo12.jpg";
+
 import { TbZoomIn } from "react-icons/tb";
 import { FaLink } from "react-icons/fa6";
 import {
@@ -27,18 +42,18 @@ import {
 
 function MyWorks() {
   const myworks = [
-    { id: 1, category: "app", title: "App 1", img: app1 },
-    { id: 2, category: "product", title: "Product 1", img: app2 },
-    { id: 3, category: "branding", title: "Branding 1", img: app3 },
-    { id: 4, category: "books", title: "Books 1", img: app4 },
-    { id: 5, category: "app", title: "App 2", img: app5 },
-    { id: 6, category: "product", title: "Product 2", img: app6 },
-    { id: 7, category: "branding", title: "Branding 2", img: app7 },
-    { id: 8, category: "books", title: "Books 2", img: app8 },
-    { id: 9, category: "app", title: "App 3", img: app9 },
-    { id: 10, category: "product", title: "Product 3", img: app10 },
-    { id: 11, category: "branding", title: "Branding 3", img: app11 },
-    { id: 12, category: "books", title: "Books 3", img: app12 },
+    { id: 1, category: "app", title: "App 1", img: photo1 },
+    { id: 2, category: "product", title: "Product 1", img: photo2 },
+    { id: 3, category: "branding", title: "Branding 1", img: photo3 },
+    { id: 4, category: "books", title: "Books 1", img: photo4 },
+    { id: 5, category: "app", title: "App 2", img: photo5 },
+    { id: 6, category: "product", title: "Product 2", img: photo6 },
+    { id: 7, category: "branding", title: "Branding 2", img: photo7 },
+    { id: 8, category: "books", title: "Books 2", img: photo8 },
+    { id: 9, category: "app", title: "App 3", img: photo9 },
+    { id: 10, category: "product", title: "Product 3", img: photo10 },
+    { id: 11, category: "branding", title: "Branding 3", img: photo11 },
+    { id: 12, category: "books", title: "Books 3", img: photo12 },
   ];
 
   const [selectedCategory, setSelectedCategory] = useState("all");
